05544f1e8aeec0d80d632a4f4cac196388893e30
Ivan Rudakov <ivan.rudakov@umbrellait.com>
2024-11-19 23:56:33 +0300

    LAST?

--- script.sh
+++ script.sh
@@ -4,15 +4,23 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 
 while true; do
     if php wp-cli.phar core is-installed --allow-root 2>/dev/null; then
-        if [ -d "/var/www/html/wp-content/plugins/redis-cache" ]; then
-            echo "Redis Cache plugin is OK."
-            break
-        else
-            php wp-cli.phar plugin install redis-cache --allow-root
-            php wp-cli.phar plugin activate redis-cache --allow-root
-            php wp-cli.phar redis enable --allow-root
+        if php wp-cli.phar plugin is-active redis-cache --allow-root 2>/dev/null && php wp-cli.phar redis status --allow-root 2>/dev/null | grep -q "Status: Connected"; then
+            echo "Redis Cache plugin is already installed, active, and enabled. Exiting..."
             break
         fi
+
+        if [ ! -d "/var/www/html/wp-content/plugins/redis-cache" ]; then
+            echo "Installing Redis Cache plugin..."
+            php wp-cli.phar plugin install redis-cache --allow-root
+        fi
+
+        echo "Activating Redis Cache plugin..."
+        php wp-cli.phar plugin activate redis-cache --allow-root
+
+        echo "Enabling Redis Cache..."
+        php wp-cli.phar redis enable --allow-root
+
+        echo "Redis Cache plugin has been installed, activated, and enabled. Exiting..."
         break
     else
         echo "WordPress not installed. Retrying in 30 seconds..."
