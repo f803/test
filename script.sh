f3eba4e63889370d9732e128a42e7f2ebacbfc90
Ivan Rudakov <ivan.rudakov@umbrellait.com>
2024-11-19 15:52:46 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,8 +2,6 @@
 
 while true; do
     if php wp-cli.phar core is-installed --allow-root 2>/dev/null; then
-        # WP is installed. Let's do some things we should only do in a confirmed WP environment.
-        curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
         php wp-cli.phar plugin install redis-cache --allow-root
         php wp-cli.phar plugin activate redis-cache --allow-root
         php wp-cli.phar redis enable --allow-root
