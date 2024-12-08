029899543578eac216a48a4b97247980c86bfa21
Ivan Rudakov <ivan.rudakov@umbrellait.com>
2024-12-08 23:14:46 +0300

    asd

--- wordpress.sh
+++ wordpress.sh
@@ -40,15 +40,15 @@ while true; do
         # Установка и настройка OpenID Connect
         if [ ! -d "/var/www/html/wp-content/plugins/openid-connect-generic" ]; then
             echo "Installing OpenID Connect plugin..."
-            su -s /bin/bash www-data -c "php /var/www/html/wp-cli.phar plugin install openid-connect-generic"
+            su -s /bin/bash www-data -c "php /var/www/html/wp-cli.phar plugin install daggerhart-openid-connect-generic"
         fi
 
         echo "Activating OpenID Connect plugin..."
-        su -s /bin/bash www-data -c "php /var/www/html/wp-cli.phar plugin activate openid-connect-generic"
+        su -s /bin/bash www-data -c "php /var/www/html/wp-cli.phar plugin activate daggerhart-openid-connect-generic"
 
         echo "Setting OpenID Connect plugin configuration..."
-        su -s /bin/bash www-data -c "php /var/www/html/wp-cli.phar option update openid_connect_generic_settings \
-            'a:26:{s:10:\"login_type\";s:4:\"auto\";s:9:\"client_id\";s:9:\"wordpress\";s:13:\"client_secret\";s:32:\"rZkIDbc2mVzTC2jmAFEPH1rso1d5czZW\";s:5:\"scope\";s:20:\"openid profile email\";s:14:\"endpoint_login\";s:70:\"http://192.168.4.50:8443/realms/wordpress/protocol/openid-connect/auth\";s:17:\"endpoint_userinfo\";s:70:\"http://keycloak:8443/realms/wordpress/protocol/openid-connect/userinfo\";s:14:\"endpoint_token\";s:67:\"http://keycloak:8443/realms/wordpress/protocol/openid-connect/token\";s:20:\"endpoint_end_session\";s:72:\"http://192.168.4.62:8443/realms/wordpress/protocol/openid-connect/logout\";s:10:\"acr_values\";s:0:\"\";s:12:\"identity_key\";s:18:\"preferred_username\";s:12:\"no_sslverify\";s:1:\"1\";s:20:\"http_request_timeout\";s:1:\"5\";s:15:\"enforce_privacy\";s:1:\"0\";s:22:\"alternate_redirect_uri\";s:1:\"0\";s:12:\"nickname_key\";s:18:\"preferred_username\";s:12:\"email_format\";s:7:\"{email}\";s:18:\"displayname_format\";s:0:\"\";s:22:\"identify_with_username\";s:1:\"0\";s:16:\"state_time_limit\";s:3:\"180\";s:20:\"token_refresh_enable\";s:1:\"1\";s:19:\"link_existing_users\";s:1:\"1\";s:24:\"create_if_does_not_exist\";s:1:\"1\";s:18:\"redirect_user_back\";s:1:\"1\";s:18:\"redirect_on_logout\";s:1:\"1\";s:14:\"enable_logging\";s:1:\"0\";s:9:\"log_limit\";s:4:\"1000\";}'"
+        su -s /bin/bash www-data -c "php /var/www/html/wp-cli.phar option update daggerhart-openid-connect-generic \
+            'a:26:{s:10:\"login_type\";s:4:\"auto\";s:9:\"client_id\";s:9:\"wordpress\";s:13:\"client_secret\";s:32:\"rZkIDbc2mVzTC2jmAFEPH1rso1d5czZW\";s:5:\"scope\";s:20:\"openid profile email\";s:14:\"endpoint_login\";s:70:\"http://sashayanebalbes.ddns.net//realms/wordpress/protocol/openid-connect/auth\";s:17:\"endpoint_userinfo\";s:70:\"http://keycloak:8443/realms/wordpress/protocol/openid-connect/userinfo\";s:14:\"endpoint_token\";s:67:\"http://keycloak:8443/realms/wordpress/protocol/openid-connect/token\";s:20:\"endpoint_end_session\";s:72:\"http://192.168.4.62:8443/realms/wordpress/protocol/openid-connect/logout\";s:10:\"acr_values\";s:0:\"\";s:12:\"identity_key\";s:18:\"preferred_username\";s:12:\"no_sslverify\";s:1:\"1\";s:20:\"http_request_timeout\";s:1:\"5\";s:15:\"enforce_privacy\";s:1:\"0\";s:22:\"alternate_redirect_uri\";s:1:\"0\";s:12:\"nickname_key\";s:18:\"preferred_username\";s:12:\"email_format\";s:7:\"{email}\";s:18:\"displayname_format\";s:0:\"\";s:22:\"identify_with_username\";s:1:\"0\";s:16:\"state_time_limit\";s:3:\"180\";s:20:\"token_refresh_enable\";s:1:\"1\";s:19:\"link_existing_users\";s:1:\"1\";s:24:\"create_if_does_not_exist\";s:1:\"1\";s:18:\"redirect_user_back\";s:1:\"1\";s:18:\"redirect_on_logout\";s:1:\"1\";s:14:\"enable_logging\";s:1:\"0\";s:9:\"log_limit\";s:4:\"1000\";}'"
 
         echo "Plugins are installed, activated, and configured. Exiting..."
         break
