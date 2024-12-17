c13b6457144b13b5f4257e91fef1d431aaa13200
f803 <70684453+f803@users.noreply.github.com>
2024-12-17 11:41:17 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -11,11 +11,11 @@ else
 fi
 
 chown -R www-data:www-data /var/www/html
-find /var/www/html -type d -exec chmod 755 {} \;
-find /var/www/html -type f -exec chmod 644 {} \;
+find /var/www/html -type d -exec chmod 775 {} \;
+find /var/www/html -type f -exec chmod 664 {} \;
 chown www-data:www-data /var/www/html/wp-cli.phar
-chmod 755 /var/www/html/wp-cli.phar
-
+chmod 775 /var/www/html/wp-cli.phar
+find /var/www/html -type d -exec chmod g+s {} \;
 
 while true; do
     if su -s /bin/bash www-data -c "php /var/www/html/wp-cli.phar core is-installed" 2>/dev/null; then
